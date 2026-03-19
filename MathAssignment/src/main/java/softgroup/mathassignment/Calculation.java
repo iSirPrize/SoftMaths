86bb6959cc8d80065a7d22f982eaaa32fb8409d4
Quentin <masoe.quentin@gmail.com>
2026-03-19 21:13:48 +1300

    added user functionality

--- MathAssignment/src/main/java/softgroup/mathassignment/Calculation.java
+++ MathAssignment/src/main/java/softgroup/mathassignment/Calculation.java
@@ -4,10 +4,32 @@
  */
 package softgroup.mathassignment;
 
+import java.math.BigInteger;
+
 /**
  *
  * @author Prize
  */
-public class Calculation {
+public class Calculation 
+{
+    private BigInteger p;
+    private BigInteger g;
+    private BigInteger personalKey;
     
+    public Calculation(BigInteger p, BigInteger g, BigInteger personalKey)
+    {
+        this.p = p;
+        this.g = g;
+        this.personalKey = personalKey;
+    }
+    
+    public BigInteger getPublicKey()
+    {
+        return g.modPow(personalKey, p);
+    }
+    
+    public BigInteger cipherKey(BigInteger key)
+    {
+        return key.modPow(personalKey, p);
+    }    
 }
--- MathAssignment/src/main/java/softgroup/mathassignment/MathAssignment.java
+++ MathAssignment/src/main/java/softgroup/mathassignment/MathAssignment.java
@@ -4,13 +4,39 @@
 
 package softgroup.mathassignment;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 /**
  *
  * @author Prize
  */
-public class MathAssignment {
+public class MathAssignment 
+{
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Public p: ");
+        BigInteger p = new BigInteger(sc.next());
+        
+        System.out.print("\nPublic g: ");
+        BigInteger g = new BigInteger(sc.next());
+        
+        System.out.print("\nIntput personal key: ");
+        BigInteger personalKey = new BigInteger(sc.next());
+        Calculation keyCalc = new Calculation(p, g, personalKey);
+        
+        //get public key to share with confidant
+        BigInteger shareKey = keyCalc.getPublicKey();
+        
+        System.out.print("\nPublic key is: " + shareKey);
+        
+        System.out.print("\nInput public key recieved from confidant: ");
+        BigInteger recievedKey = new BigInteger(sc.next());
+        sc.close();
+        
+        System.out.print("\nCipher key result: " + keyCalc.cipherKey(recievedKey));
     }
 }
